2c616a133e930ce9efbbef1a1e451b29a75d11ba
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-14 14:06:06 +0400

    progress

--- src/home.js
+++ src/home.js
@@ -1,4 +1,8 @@
 import image from "./images/chef.jpg";
+import food1 from "./images/food1.jpg";
+import food2 from "./images/food2.jpg";
+import food3 from "./images/food3.jpg";
+import food4 from "./images/food4.jpg";
 
 function page() {
 		let mainContent = document.createElement("div");
@@ -17,8 +21,33 @@ function page() {
 		heroPage.appendChild(el);
 	
 		mainContent.appendChild(heroPage);
+
+
+		let secondCard = document.createElement("div");
+		el = document.createElement("div");
+		let subEl = document.createElement("div");
+		
+		let imgEl, subP, i;
+		for (i=0; i < 4; i++) {
+			subEl.className = "foodCard";
+			imgEl = document.createElement("img");
+			imgEl.setAttribute("src", [food1, food2, food3, food4][i]);
+			subEl.appendChild(imgEl);
+			subP = document.createElement("p");
+			subP.textContent = `Food ${i}`;
+			subEl.appendChild(subP);
+			el.appendChild(subEl);
+		}
+		mainContent.append(el);
+	
 		return mainContent;
 }
 
 export default page();
+
+
+
+
+
+
 
