6f3ab64e92ea47d76e7d60c48fc8033704450471
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-14 06:42:02 +0400

    workin on restarurantPage

--- src/index.js
+++ src/index.js
@@ -2,20 +2,38 @@ import home from "./home.js"
 import menu from "./menu.js"
 import about from "./about.js"
 
-const navEl = document.querySelector("header nav");
-const contentEl = document.querySelector(".content");
+let navEl = document.querySelector("header nav");
+let contentEl = document.querySelector("#content");
 
 navEl.addEventListener("click", (e) => {
-		if (e.target.className == "homeBtn") updateDom(home);
-		if (e.target.className == "menuBtn") updateDom(menu);
-		if (e.target.className == "aboutBtn") updateDom(about);
+		if (e.target.className == "homeBtn") {
+			btnStyleToggle();			
+			updateDom(home);
+		} else if (e.target.className == "menuBtn") {
+			btnStyleToggle();			
+			updateDom(menu);
+		} else if (e.target.className == "aboutBtn") {
+			btnStyleToggle();			
+			updateDom(about);
+		}
 });
 
-const updateDom = (page) => {
-	contentEl.innerHTML = "";
+// to toggle css to change button when active *not complete*
+let btnStyleToggle = (name) => {
+	let navArr = navEl.querySelectorAll("button");
+	let classArr = [];
+	for (let i=0; i < navArr.length; i++) {
+		classArr.push(navArr[i].className);
+	}	
+	console.log(classArr);
+}
+
+let updateDom = (page) => {
+	contentEl.innerHTML = ""
 	contentEl.appendChild(page);
 
 };
 
+updateDom(home);
 
 
