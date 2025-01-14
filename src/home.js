d48eb62ae4a8fdb2c56fa55753ba26399ee411f9
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-14 10:35:26 +0400

    progress

--- src/home.js
+++ src/home.js
@@ -1,8 +1,14 @@
+import image from "./images/chef.jpg";
+
 function page() {
 		let heroPage = document.createElement("div");
 		heroPage.className = "card dark";
 		let el = document.createElement("h1");
-		el.textContent = "Authentic American Sushi";
+		el.textContent = "Authentic Sushi Restaurant";
+		heroPage.appendChild(el);
+		el = document.createElement("img");
+		el.setAttribute("src", image);
+		el.className = "chefImage";
 		heroPage.appendChild(el);
 		return heroPage;
 }
