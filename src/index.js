57d13228651054506197213180404cb63a157fdf
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-14 06:10:38 +0400

    working on restaurantPage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("hello");
+import home from "./home.js"
+import menu from "./menu.js"
+import about from "./about.js"
+
+const navEl = document.querySelector("header nav");
+const contentEl = document.querySelector(".content");
+
+navEl.addEventListener("click", (e) => {
+		if (e.target.className == "homeBtn") updateDom(home);
+		if (e.target.className == "menuBtn") updateDom(menu);
+		if (e.target.className == "aboutBtn") updateDom(about);
+});
+
+const updateDom = (page) => {
+	contentEl.innerHTML = "";
+	contentEl.appendChild(page);
+
+};
+
+
+
