af1b6032a5c9a5b7651b37aa205cce2cbb79792c
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-01-14 04:49:34 +0400

    install npm packages and set up webkit config files

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// webpack.confid.js
+
+modeule.export = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true;
+	},
+	devtool: "eval-source-map",
+	devServer: {
+		watchFiles: ["./src/template.html"],
+	},
+	module: {
+		rules: [
+			{	
+				test: /\.css$/i,
+				user: ["style-loader", "css-loader"],
+			},
+		],
+	},
+}; 
